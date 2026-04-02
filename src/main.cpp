31b3f4971f635191913ef3380ff7d38cfee6f885
Dan Adrian Mirea <damirea84@gmail.com>
2026-04-02 09:50:32 +0300

    change resolution to a more web friendly format

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 #include <cmath>
 
-const int WINDOW_WIDTH = 1200;
-const int WINDOW_HEIGHT = 800;
+const int WINDOW_WIDTH = 960;
+const int WINDOW_HEIGHT = 540;
 const char* WINDOW_TITLE = "Lorenz Attractor Simulation";
 
 // Lorenz system parameters
